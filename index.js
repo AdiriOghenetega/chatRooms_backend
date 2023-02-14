918f15e38fc9c8a1d838f5f063ce76205e020969
Adiri Oghenetega <tegaadiri3@yahoo.com>
2023-02-14 10:38:50 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "https://chat-app-beta-weld.vercel.app/",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+    socket.emit("Disconnected",socket.id)
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("SERVER RUNNING");
+});  
