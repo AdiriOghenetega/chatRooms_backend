4253c54a1143668d635be6d686f8854d7d111f06
Adiri Oghenetega <tegaadiri3@yahoo.com>
2023-02-15 04:38:22 -0600

    updated

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const cors = require("cors");
 const { Server } = require("socket.io");
+
+const cors = require("cors");
 app.use(cors());
+
+const router = require('./router');
+app.use(router);
 
 const PORT = process.env.PORT || 3001;
 
 const server = http.createServer(app);
 
-const io = new Server(server);
+const io = new Server(server,{
+  cors: {
+    origin: "https://chat-app-beta-weld.vercel.app/"
+  }
+});
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
@@ -30,5 +38,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(PORT, () => {
-  console.log("SERVER RUNNING");
+  console.log(`SERVER RUNNING on port ${PORT}`);
 });  
