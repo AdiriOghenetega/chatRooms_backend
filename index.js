4b60e581804ebd952a6288c8ce448429be7c4561
Adiri Oghenetega <tegaadiri3@yahoo.com>
2023-02-15 04:53:49 -0600

    new

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
   cors: {
-    origin: "https://chat-app-beta-weld.vercel.app/"
+    origin: "https://chat-app-beta-weld.vercel.app"
   }
 });
 
